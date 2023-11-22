b1430de0961c599a7bef200d5ca908c31ad50866
Franky Wijanarko <frank.jr.619@gmail.com>
2023-11-22 11:15:54 +0700

    create onboarding components = selectable button and unit text field

---
+++ onboarding/presentation/src/main/java/com/fjr619/onboarding/presentation/component/SelectableButton.kt
@@ -0,0 +1,48 @@
+package com.fjr619.onboarding.presentation.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SelectableButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    isSelected: Boolean,
+    color: Color,
+    selectedTextColor: Color,
+    style: TextStyle = MaterialTheme.typography.labelLarge,
+    onClick: ()-> Unit
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.outlinedButtonColors(
+            containerColor = if (isSelected) color else Color.Transparent,
+            contentColor = if (isSelected) selectedTextColor else color
+        ),
+        border = BorderStroke(1.dp, color)
+    ) {
+        Text(text = text, style = style)
+    }
+}
+
+@Composable
+@Preview
+fun PreviewSelectableButton() {
+    SelectableButton(
+        text = "AAAa",
+        isSelected = true,
+        color = Color.Green,
+        selectedTextColor = Color.White,
+        onClick = {}
+    )
+}
---
+++ onboarding/presentation/src/main/java/com/fjr619/onboarding/presentation/component/UnitTextField.kt
@@ -0,0 +1,55 @@
+package com.fjr619.onboarding.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+import com.fjr619.core.ui.LocalSpacing
+
+@Composable
+fun UnitTextField(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    unit: String,
+    style: TextStyle = TextStyle(
+        color = MaterialTheme.colorScheme.secondary,
+        fontSize = 70.sp
+    )
+) {
+    val spacing = LocalSpacing.current
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = style,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            singleLine = true,
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .alignBy(LastBaseline)
+        )
+        Spacer(modifier = Modifier.width(spacing.spaceSmall))
+        Text(
+            text = unit,
+            modifier = Modifier.alignBy(LastBaseline)
+        )
+    }
+}
